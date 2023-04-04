51146d84096bb4247fbb035aad58bb68a36cc611
penggan19 <1780740894@qq.com>
2023-04-04 20:15:05 +0800

    “彭淦提交release描述”

---
+++ src/system.ts
@@ -0,0 +1,28 @@
+import type { Model } from "./types"
+
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "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",
+  openaiAPITemperature: 60,
+  password: "",
+  systemRule: "",
+  model: "gpt-3.5-turbo" as Model
+}
+
+export const defaultMessage = `Powered by OpenAI Vercel
+- 本项目二次开发，个人维护，疑问请 [主页留言] (https://penggan19.com/)。
+- 本网站仅作为项目演示，不提供服务，也可填入自己的 Key。
+- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
+
+export type Setting = typeof defaultSetting
+
+export const defaultResetContinuousDialogue = false
+
+export const defaultMaxInputTokens: Record<Model, number> = {
+  "gpt-3.5-turbo": 3072,
+  "gpt-4": 6144,
+  "gpt-4-32k": 24576
+}
+
+export const defaultModel: Model = "gpt-3.5-turbo"
